b40c00528cfba26422d8607242a4aae97f3bfea7
Tim Shortt <tshortt@redroverk12.com>
2026-01-23 13:55:35 -0500

    just trying out a manual change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('renders the task manager heading', () => {
+  it('renders the starter text', () => {
     render(<App />)
     expect(screen.getByText('hello world')).toBeInTheDocument()
   })
