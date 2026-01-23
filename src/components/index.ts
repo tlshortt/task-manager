1bbe6916ec24ac7f1e53ea0987099e05509c862d
Tim Shortt <tshortt@redroverk12.com>
2026-01-23 13:46:46 -0500

    remove example code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,6 @@
 // Component exports
 // Ralph will add more component exports as features are built
 
-export { TaskList } from './TaskList'
-export { TaskCard } from './TaskCard'
-export { TaskForm } from './TaskForm'
-
 // TODO: Ralph will add these exports:
 // export { PriorityBadge } from './PriorityBadge'   (US-003)
 // export { PrioritySelect } from './PrioritySelect' (US-004)
