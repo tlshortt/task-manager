d8de2642331aabbc845f62c424f966c0b0124106
Tim Shortt <tshortt@redroverk12.com>
2026-01-23 13:52:25 -0500

    start with working test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 describe('App', () => {
   it('renders the task manager heading', () => {
     render(<App />)
-    expect(screen.getByText('Task Manager')).toBeInTheDocument()
+    expect(screen.getByText('hello world')).toBeInTheDocument()
   })
 })
